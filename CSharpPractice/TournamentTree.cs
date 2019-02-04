416285d9ae8832cea351ee0ac9918a24d08d3d36
Dylan Guidry <guidry.dylan.95@gmail.com>
2019-02-04 14:57:01 -0600

    Tournament Tree

---
+++ CSharpPractice/TournamentTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPractice
+{
+    struct Node<T>
+    {
+        public T Value;
+        public bool IsVictor;
+    }
+
+    class Match<T>
+    {
+        public Node<T> ContestantLeft;
+        public Node<T> ContestantRight;
+
+        public Match<T> ConnectedMatchLeft;
+        public Match<T> ConnectedMatchRight;
+
+        void SetLeftContestant()
+        {
+            ContestantLeft.Value = ConnectedMatchLeft.GetVictor();
+        }
+        void SetRightContestant()
+        {
+            ContestantRight.Value = ConnectedMatchRight.GetVictor();
+        }
+
+        T GetVictor()
+        {
+            return ContestantLeft.IsVictor ? ContestantLeft.Value : ContestantRight.Value;
+        }
+
+    }
+
+    class TournamentTree
+    {
+    }
+}
