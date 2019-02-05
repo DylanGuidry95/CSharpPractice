ad27c4f6d58169441cacb2e042a7a2a5905ccd67
Dylan Guidry <guidry.dylan.95@gmail.com>
2019-02-05 13:06:37 -0600

    Decimal To Roman Numeral

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpPractice
 {
@@ -7,6 +8,72 @@ namespace CSharpPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //I = 1
+            //V = 5
+            //L = 10
+            //C = 100
+            //D = 500
+            //M = 1000
+
+            //Decimal Values
+            //1 = I
+            //4 = IV
+            //5 = V
+            //9 = IX
+            //10 = X
+            //40 = XL
+            //50 = L
+            //90 = XC
+            //100 = C
+            //400 = CD
+            //500 = D
+            //900 = CM
+            //1000 = M
+
+            Dictionary<int, string> DecToRoman = new Dictionary<int, string>
+            {
+                { 1, "I" },
+                { 4, "IV" },
+                { 5, "V" },
+                { 9, "IX" },
+                { 10, "X" },
+                { 40, "XL" },
+                { 50, "L" },
+                { 90, "XC" },
+                { 100, "C" },
+                { 400, "CD" },
+                { 500, "D" },
+                { 900, "CM" },
+                { 1000, "M" }
+            };
+
+            int UserValue = 100;
+            string RomanValue = "";
+
+            int curValue = UserValue;
+            while (curValue > 0)
+            {
+                KeyValuePair<int, string> lastLowest;
+                if (DecToRoman.ContainsKey(curValue))
+                {
+                    lastLowest = 
+                        new KeyValuePair<int, string>(curValue,DecToRoman[curValue]);
+                    curValue = curValue - lastLowest.Key;
+                    RomanValue += lastLowest.Value;
+                    break;                    
+                }
+
+                foreach (var kvp in DecToRoman)
+                {
+                    if(kvp.Key > curValue)
+                        break;
+                    lastLowest = kvp;
+                }
+                curValue = curValue - lastLowest.Key;
+                RomanValue += lastLowest.Value;
+            }
+            Console.WriteLine(RomanValue);
+            Console.ReadKey();
         }
     }
 }
